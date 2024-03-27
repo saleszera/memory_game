52014ddd386e4ea94c0908a54a08dfd9fe640ca6
Raniery Sales <rany_sales@hotmail.com>
2024-03-27 18:19:46 -0300

    feat: configs related of storybook

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // Learn more https://docs.expo.io/guides/customizing-metro
+const { generate } = require('@storybook/react-native/scripts/generate');
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+generate({
+  configPath: path.resolve(__dirname, './.storybook'),
+});
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.transformer.unstable_allowRequireContext = true;
+
+config.resolver.sourceExts.push('mjs');
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  const defaultResolveResult = context.resolveRequest(
+    context,
+    moduleName,
+    platform,
+  );
+
+  if (
+    process.env.STORYBOOK_ENABLED !== 'true' &&
+    Boolean(defaultResolveResult?.filePath?.includes?.('.storybook/'))
+  ) {
+    return {
+      type: 'empty',
+    };
+  }
+
+  return defaultResolveResult;
+};
 
 module.exports = config;
